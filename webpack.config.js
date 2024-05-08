306e9258d421f5cc0c99c590743ecc93b34264fa
Nick Drian <nickdrian@Nicks-MacBook-Air.local>
2024-05-07 19:43:55 -0600

    run script

--- webpack.config.js
+++ webpack.config.js
@@ -5,50 +5,50 @@ const webpack = require('webpack');
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 // change this to run different examples
-const name = "example_01";
+module.exports = env => {
+  return {
+    entry: [`./${env.name}/app.js`],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'app.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
 
-module.exports = {
-  entry: [`./${name}/app.js`],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+    devServer: {
+      client: {
+        //prevent control flow error from showing on screen
+        overlay: false
+      }
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: `./${env.name}/index.html`
+      }),
+      new WasmPackPlugin({
+        crateDirectory: path.resolve(__dirname, "./engine/")
+      }),
+      // Have this example work in Edge which doesn't ship `TextEncoder` or
+      // `TextDecoder` at this time.
+      new webpack.ProvidePlugin({
+        TextDecoder: ['text-encoding', 'TextDecoder'],
+        TextEncoder: ['text-encoding', 'TextEncoder']
+      })
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-
-  devServer: {
-    client: {
-      //prevent control flow error from showing on screen
-      overlay: false
+    mode: 'development',
+    experiments: {
+      asyncWebAssembly: true
     }
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: `./${name}/index.html`
-    }),
-    new WasmPackPlugin({
-      crateDirectory: path.resolve(__dirname, "./engine/")
-    }),
-    // Have this example work in Edge which doesn't ship `TextEncoder` or
-    // `TextDecoder` at this time.
-    new webpack.ProvidePlugin({
-      TextDecoder: ['text-encoding', 'TextDecoder'],
-      TextEncoder: ['text-encoding', 'TextEncoder']
-    })
-  ],
-  mode: 'development',
-  experiments: {
-    asyncWebAssembly: true
   }
 };
