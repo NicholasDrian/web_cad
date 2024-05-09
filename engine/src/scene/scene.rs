0bb96137a926263b6d64793973ed425c69e8c893
Nick Drian <nickdrian@Nicks-MacBook-Air.local>
2024-05-08 22:02:00 -0600

    created scene

--- engine/src/scene/scene.rs
+++ engine/src/scene/scene.rs
@@ -1,7 +1,36 @@
-pub struct Scene {}
+use std::collections::HashMap;
+
+use crate::geometry::{
+    curve::Curve, geometry_id::GeometryId, mesh::Mesh, polyline::Polyline, surface::Surface,
+};
+
+pub struct Scene {
+    curves: HashMap<GeometryId, Curve>,
+    surfaces: HashMap<GeometryId, Surface>,
+    polylines: HashMap<GeometryId, Polyline>,
+    meshes: HashMap<GeometryId, Mesh>,
+}
 
 impl Scene {
     pub fn new() -> Scene {
-        Scene {}
+        Scene {
+            curves: HashMap::new(),
+            surfaces: HashMap::new(),
+            polylines: HashMap::new(),
+            meshes: HashMap::new(),
+        }
+    }
+
+    pub fn get_curves(&self) -> &HashMap<GeometryId, Curve> {
+        &self.curves
+    }
+    pub fn get_surfaces(&self) -> &HashMap<GeometryId, Surface> {
+        &self.surfaces
+    }
+    pub fn get_polylines(&self) -> &HashMap<GeometryId, Polyline> {
+        &self.polylines
+    }
+    pub fn get_meshes(&self) -> &HashMap<GeometryId, Mesh> {
+        &self.meshes
     }
 }
